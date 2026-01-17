5dce90d0ecaca4956725301e1eca73e9c7c752d6
ahmeddouroum <ahmeddouroum@gmail.com>
2026-01-17 14:44:49 +0100

    With fact Iterative function

--- combinations.py
+++ combinations.py
@@ -1,11 +1,12 @@
+def fact(n):
+    result = 1
+    for i in range(2, n + 1):
+        result *= i
+    return result
+
 def n_choose_k(n, k):
     if k < 0 or k > n:
         return 0
-    if k == 0 or k == n:
-        return 1
-    result = 1
-    for i in range(1, k+1):
-        result = result * (n - i + 1) // i
-    return result
+    return fact(n) // (fact(k) * fact(n - k))
 
 print(n_choose_k(n, k))
