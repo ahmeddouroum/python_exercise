7b79355d8425e995b1883950f509437eb8c643cd
ahmeddouroum <ahmeddouroum@gmail.com>
2026-01-17 14:46:02 +0100

    Using the math library

--- combinations.py
+++ combinations.py
@@ -1,11 +1,6 @@
-def fact(n):
-    if n == 0 or n == 1:
-        return 1
-    return n * fact(n - 1)
+import math
 
 def n_choose_k(n, k):
-    if k < 0 or k > n:
-        return 0
-    return fact(n) // (fact(k) * fact(n - k))
+    return math.comb(n, k)
 
 print(n_choose_k(n, k))
